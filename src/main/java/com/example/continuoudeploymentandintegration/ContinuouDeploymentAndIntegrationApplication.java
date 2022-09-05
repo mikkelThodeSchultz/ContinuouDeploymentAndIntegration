6e6c66ae4af837849c260738a737b7e15bc6296b
mikkelThodeSchultz <mikk75c4@stud.kea.dk>
2022-09-05 13:29:15 +0200

    HelloWorld

--- src/main/java/com/example/continuoudeploymentandintegration/ContinuouDeploymentAndIntegrationApplication.java
+++ src/main/java/com/example/continuoudeploymentandintegration/ContinuouDeploymentAndIntegrationApplication.java
@@ -8,6 +8,7 @@ public class ContinuouDeploymentAndIntegrationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ContinuouDeploymentAndIntegrationApplication.class, args);
+        System.out.println("HelloWorld");
     }
 
 }
