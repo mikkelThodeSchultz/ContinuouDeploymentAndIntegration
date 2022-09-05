58200854632660e239cb8b0e4d850bdf79d105b3
mikkelThodeSchultz <mikk75c4@stud.kea.dk>
2022-09-05 13:13:24 +0200

    commit

---
+++ src/main/java/com/example/continuoudeploymentandintegration/ContinuouDeploymentAndIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.continuoudeploymentandintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuouDeploymentAndIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContinuouDeploymentAndIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/continuoudeploymentandintegration/ContinuouDeploymentAndIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.continuoudeploymentandintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinuouDeploymentAndIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
